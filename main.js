5427ae0df7ea8dc14ebdd47e987ca43a87a922f7
Flower123456789 <82616100+Flower123456789@users.noreply.github.com>
2021-12-10 15:37:03 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 img = "";
 status = "";
+objects = [];
 
 
 function preload() {
@@ -17,18 +18,21 @@ function setup() {
 
 function draw() {
     image( img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect( 30, 60, 450, 350);
 
+    if(status != "") {
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect( 300, 90, 270, 320);
+        for( i = 0; i < objects.length; i++ ) {
+
+            document.getElementById("status").innerHTML = " Status: Object Detected";
+            
+            fill("#03f4fc");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "  " + percent + "%", objects[i].x ,objects[i].y );
+            noFill();
+            stroke("royalblue");
+            rect(objects[i].x ,objects[i].y ,objects[i].width ,objects[i].height);
+        } 
+    }
 
 }
 
@@ -49,4 +53,5 @@ function gotResults( error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
